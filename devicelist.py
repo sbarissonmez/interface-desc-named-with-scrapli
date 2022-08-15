c87e0f93eb06d85c92b799828dda8ce2973bf2cb
Baris <s.barissonmez@gmail.com>
2022-08-15 23:10:05 +0300

    maincode

---
+++ devicelist.py
@@ -0,0 +1,7 @@
+devices = [
+    {"host": "x.x.x.x", "auth_username": "admin", "auth_password": "password", "ssh_config_file": True, "auth_strict_key": False},
+    {"host": "x.x.x.x", "auth_username": "admin", "auth_password": "password", "ssh_config_file": True, "auth_strict_key": False},
+    {"host": "x.x.x.x", "auth_username": "admin", "auth_password": "password", "ssh_config_file": True, "auth_strict_key": False},
+    {"host": "x.x.x.x", "auth_username": "admin", "auth_password": "password", "ssh_config_file": True, "auth_strict_key": False},
+    {"host": "x.x.x.x", "auth_username": "admin", "auth_password": "password", "ssh_config_file": True, "auth_strict_key": False}
+]
---
+++ scrapli-cdp.py
@@ -0,0 +1,19 @@
+from urllib import response
+from scrapli.driver.core import IOSXEDriver
+from devicelist import devices
+from rich import print as rp
+
+for device in devices:
+    with IOSXEDriver(**device) as conn:
+        response = conn.send_command("show cdp neighbor detail")
+        structured_response = response.genie_parse_output()
+        cdp_index = structured_response['index']
+        rp(f"********** Sending Configurations to {device['host']} **********\n")
+        for id in cdp_index:
+            remote_device = cdp_index[id]['device_id'].split('.')[0]
+            remote_port = cdp_index[id]['port_id']
+            local_port = cdp_index[id]['local_interface']
+            config_commands = [f"interface {local_port}", f"description Connected to {remote_device}'s {remote_port}"]
+            results = conn.send_configs(config_commands)
+            rp(results.result)
+        rp("\n")
